efb5f50d93a1bf0be8875a872f1bef2bd113d7f1
sujoy ghoshal <92462200+sujoyghoshal@users.noreply.github.com>
2024-02-22 17:53:39 +0530

    Add files via upload

---
+++ ToDO-App-crete/index.js
@@ -0,0 +1,39 @@
+const inputBox = document.getElementById('input-box');
+const listContainer = document.getElementById('list-container');
+const button = document.getElementById('add-btn');
+//*Add 
+button.onclick = () => {
+    //alert('hi');
+    if (inputBox.value === '') {
+        alert('You must write something');
+    }
+    else {
+        let li = document.createElement('li');
+        li.innerHTML = inputBox.value;
+        listContainer.append(li);
+        let span = document.createElement('span');
+        span.innerHTML = "\u00d7"; //x
+        li.appendChild(span);
+    }
+    inputBox.value = "";
+    saveData()
+}
+//*Deleted And Chaked ...
+listContainer.addEventListener('click', (e) => {
+    if (e.target.tagName === "LI") {
+        e.target.classList.toggle('chaked');
+        saveData()
+    }
+    else if (e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+        saveData()
+    }
+}, false);
+
+function saveData(){
+    localStorage.setItem("data",listContainer.innerHTML);
+}
+function showTask(){
+    listContainer.innerHTML=localStorage.getItem('data');
+}
+showTask();
